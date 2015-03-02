dd1936835ee8c46f64f95eb1dc484eac6ae52ffb
lalyos <lalyos@yahoo.com>
2015-03-02 19:07:18 +0100

    seq-update updates binary from latest circle

--- cmds/sequence.bash
+++ cmds/sequence.bash
@@ -4,9 +4,8 @@ init() {
 	cmd-export seq-update
 }
 
-seq-update() {
-  declare desc="Updates the SequenceIQ glidergun modules"
 
+seq_update_modules() {
   if [[ -d $GUN_ROOT/.git ]]; then
       local remote=$(git remote -v|head -1|sed "s/^.*git@/git@/; s/ .*//")
       debug updating $GUN_ROOT from $remote
@@ -15,4 +14,17 @@ seq-update() {
       git pull --rebase origin master
       cd -
   fi
+}
+
+seq_update_binary() {
+    local latest=$(cci-latest glidergun)
+
+    debug latest binary: $latest
+}
+
+seq-update() {
+  declare desc="Updates the SequenceIQ glidergun modules"
+  
+  seq_update_modules
+  seq_update_binary
 }
