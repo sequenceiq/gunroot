7142d845846284009af2d77f20596313a37f4eb3
lalyos <lalyos@yahoo.com>
2015-03-10 12:42:49 +0100

    Add swarm file:// based discovery config generator
    Based on machine url <PREFIX>

--- cmds/swarm.bash
+++ cmds/swarm.bash
@@ -1,8 +1,27 @@
 init() {
     deps-require swarm
+    deps-require docker-machine
     
     cmd-export-ns swarm "Swarm namespace"
     cmd-export swarm-manager
+    cmd-export swarm-generate-file
+
+    : ${SWARM_DIR:=/tmp}
+}
+
+swarm-generate-file() {
+  declare desc="Generates swarm file containing docker-machine started hosts"
+  declare prefix=$1
+
+  : ${prefix:?}
+  
+  local machines=$(docker-machine ls -q | grep $prefix)
+  debug machines=$machines
+
+  debug generating $SWARM_DIR/$prefix.swarm ...
+  for m in $machines; do 
+    docker-machine url $m|sed s/^tcp...//
+  done > $SWARM_DIR/$prefix.swarm
 }
 
 swarm-manager() {
