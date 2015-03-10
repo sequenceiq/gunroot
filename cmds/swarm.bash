55daf4d7406f05917312d3a4bd1e9baa41723d96
lalyos <lalyos@yahoo.com>
2015-03-10 12:45:36 +0100

    Add swarm-get-cert-opts, backed by info harvested from `docker-machine config`

--- cmds/swarm.bash
+++ cmds/swarm.bash
@@ -5,6 +5,7 @@ init() {
     cmd-export-ns swarm "Swarm namespace"
     cmd-export swarm-manager
     cmd-export swarm-generate-file
+    cmd-export swarm-get-cert-opts
 
     : ${SWARM_DIR:=/tmp}
 }
@@ -22,6 +23,17 @@ swarm-generate-file() {
   for m in $machines; do 
     docker-machine url $m|sed s/^tcp...//
   done > $SWARM_DIR/$prefix.swarm
+}
+
+swarm-get-cert-opts() {
+  declare desc="Gets certificate options from docker-machine"
+  declare prefix=$1
+
+  : ${prefix:?}
+  local first=$(docker-machine ls -q | grep $prefix | head -1)
+
+  debug getting CERT options for $first
+  docker-machine config $first | sed "s/ /\n/g"|sed -n "/tls./ H; $ {x; s/\n/ /gp}"
 }
 
 swarm-manager() {
